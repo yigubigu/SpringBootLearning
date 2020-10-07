d573e42ff3ae8647308adbc898b08a607d5f4bbe
LoryHou <lory.hou@icloud.com>
2020-10-08 00:13:43 +0800

    Change to use ddd core

--- springboot-jsr303/src/main/java/com/itwolfed/exception/GlobalExceptionControllerAdvice.java
+++ springboot-jsr303/src/main/java/com/itwolfed/exception/GlobalExceptionControllerAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.ruize.ddd.core.gateway.ohs.ExceptionControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,21 +17,7 @@ import java.util.Map;
  */
 @Slf4j
 @RestControllerAdvice(basePackages = "com.itwolfed.controller")
-public class GlobalExceptionControllerAdvice {
+public class GlobalExceptionControllerAdvice extends com.ruize.ddd.core.gateway.ohs.ExceptionControllerAdvice {
 
-    @ExceptionHandler(value= {MethodArgumentNotValidException.class , BindException.class})
-    public Result handleVaildException(Exception e){
-        BindingResult bindingResult = null;
-        if (e instanceof MethodArgumentNotValidException) {
-            bindingResult = ((MethodArgumentNotValidException)e).getBindingResult();
-        } else if (e instanceof BindException) {
-            bindingResult = ((BindException)e).getBindingResult();
-        }
-        Map<String,String> errorMap = new HashMap<>(16);
-        bindingResult.getFieldErrors().forEach((fieldError)->
-                errorMap.put(fieldError.getField(),fieldError.getDefaultMessage())
-        );
-        return Result.build(400 , "非法参数 !" , errorMap);
-    }
 
 }
